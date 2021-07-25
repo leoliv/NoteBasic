8c7bd1132f423953532b4705002c0a9cae8f602b
Leo Oliveira <leoliv95@gmail.com>
2021-07-25 20:39:18 -0300

    Adicionado alguns botoes

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let t = document.getElementById("caixa");
+
+function negrito() {
+    t.style.fontWeight = "bold";
+}
+
+function italico() {
+    t.style.fontStyle = "italic";
+}
+
+function sublinhado() {
+    t.style.textDecoration = "underline";
+}
+
+function maiusculo() {
+    t.style.textTransform = "uppercase";
+}
+
+function menusculo() {
+    t.style.textTransform = "lowercase";
+}
