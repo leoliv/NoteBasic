5c95d057c9aae0225ae0443f101acc2f1054b157
Leo Oliveira <leoliv95@gmail.com>
2022-05-02 00:21:01 -0300

    Toggle

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
-let t = document.getElementById("caixa");
+let t = document.getElementById('caixa');
 
 function negrito() {
-    t.style.fontWeight = "bold";
+  if (t.style.fontWeight == 'bold') {
+    t.style.fontWeight = 'normal';
+    console.log('Bold');
+  } else {
+    t.style.fontWeight = 'bold';
+  }
 }
 
 function italico() {
-    t.style.fontStyle = "italic";
+  if (t.style.fontStyle == 'italic') {
+    t.style.fontStyle = 'normal';
+  } else {
+    t.style.fontStyle = 'italic';
+  }
 }
 
 function sublinhado() {
-    t.style.textDecoration = "underline";
+  if (t.style.textDecoration == 'underline') {
+    t.style.textDecoration = 'none';
+  } else {
+    t.style.textDecoration = 'underline';
+  }
 }
 
 function maiusculo() {
-    t.style.textTransform = "uppercase";
+  t.style.textTransform = 'uppercase';
 }
 
 function menusculo() {
-    t.style.textTransform = "lowercase";
+  t.style.textTransform = 'lowercase';
 }
+
